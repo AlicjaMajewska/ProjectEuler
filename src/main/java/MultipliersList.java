416558ebd9b171e491f53a9b114198fed593046c
AlicjaMajewska <alicja.majewska94@gmail.com>
2015-07-21 15:20:26 +0200

    Changed

--- src/main/java/MultipliersList.java
+++ src/main/java/MultipliersList.java
@@ -5,7 +5,7 @@ public class MultipliersList {
 
 	public static List<Integer> getListMultipliers(int multiplier) {
 		List<Integer> multiples = new ArrayList<Integer>();
-		for (int i = 0; i < 5000; ) {
+		for (int i = 0; i < 1000; ) {
 			multiples.add(new Integer(i));
 			i += multiplier;
 		}
@@ -20,6 +20,9 @@ public class MultipliersList {
 		return sum;
 	}
 	public static long  calculateListOfMultipliesOf3or5(){
-		return getSumOfList(getListMultipliers(3)) + getSumOfList(getListMultipliers(5));
+		Integer sum = new Integer(0);
+		sum += getSumOfList(getListMultipliers(3));
+		sum += getSumOfList(getListMultipliers(5));
+				return  sum;
 	}
 }
