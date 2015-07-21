90407fbf2aaf2a1eeffa36439301b27bd99afc27
AlicjaMajewska <alicja.majewska94@gmail.com>
2015-07-21 15:03:30 +0200

    Is it merged already?

--- src/main/java/MultipliersList.java
+++ src/main/java/MultipliersList.java
@@ -4,11 +4,11 @@ import java.util.List;
 public class MultipliersList {
 
 	public static List<Integer> getListMultipliers(int multiplier) {
-		List<Integer> multiplies = new ArrayList<Integer>();
+		List<Integer> multiples = new ArrayList<Integer>();
 		for (int i = 0; i < 1000; i += multiplier) {
-			multiplies.add(new Integer(i));
+			multiples.add(new Integer(i));
 		}
-		return multiplies;
+		return multiples;
 	}
 
 	public static int getSumOfList(List<Integer> integerList) {
